da3da1e7c511b46293f6956f3b5c938b18459e4c
Jocelyn Tan <jocelynpeixuan@gmail.com>
2021-10-02 15:32:23 +0800

    video call function

---
+++ src/components/Controls.vue
@@ -0,0 +1,33 @@
+<template>
+ <div class="controls">
+   <h2>Call overview</h2>
+   <hr />
+   <h3>Invite participants</h3>
+   <label for="urlInput">Share URL below to invite others</label>
+   <div>
+<!-- Room URL to copy and share -->
+     <input type="text" id="urlInput" :value="roomUrl" />
+     <button @click="copyUrl" class="teal">{{ copyButtonText }}</button>
+   </div>
+   <hr />
+   <h3>Example custom controls</h3>
+   <p>
+     You can also create your own meeting controls using daily-js methods
+   </p>
+   <div>
+     <button @click="toggleCamera">Toggle camera</button>
+     <button @click="toggleMic">Toggle mic</button>
+     <button @click="toggleScreenShare">Toggle screen share</button>
+     <button @click="expandFullscreen">Expand fullscreen</button>
+     <button @click="toggleLocalVideo">
+       {{ localVideoText }} local video
+     </button>
+     <button @click="toggleRemoteParticipants">
+       {{ remoteVideoText }} remote participants (Speaker view only)
+     </button>
+     <button @click="leaveCall">
+       Leave call
+     </button>
+   </div>
+ </div>
+</template>
---
+++ src/components/daily.vue
@@ -0,0 +1,129 @@
+<template>
+ <main class="wrapper">
+   <div class="home" v-if="status === 'home'">
+     <h2>Daily Demo</h2>
+     <p>Start demo with a new unique room or paste in your own room URL</p>
+     <div class="start-call-container">
+       <button @click="createAndJoinRoom">
+         Create room and start
+       </button>
+       <p v-if="roomError" class="error">Room could not be created</p>
+       <p class="subtext">or</p>
+       <!-- Daily room URL is entered here -->
+       <input
+         type="text"
+         placeholder="Enter room URL..."
+         v-model="roomUrl"
+         pattern="^(https:\/\/)?[\w.-]+(\.(daily\.(co)))+[\/\/]+[\w.-]+$"
+         @input="validateInput"
+       />
+       <!-- button to submit URL and join call -->
+       <button @click="submitJoinRoom" :disabled="!validRoomURL">
+         Join room
+       </button>
+     </div>
+   </div>
+
+   <div class="call-container" :class="{ hidden: status === 'home' }">
+     <!-- The Daily Prebuilt iframe is embedded in the div below using the ref -->
+     <div id="call" ref="callRef"></div>
+     <!-- Only show the control panel if a call is live -->
+     <controls
+       v-if="status === 'call'"
+       :roomUrl="roomUrl"
+       :callFrame="callFrame"
+     />
+   </div>
+ </main>
+</template>
+
+<script>
+import DailyIframe from "@daily-co/daily-js";
+import Controls from "./controls.vue";
+import api from "../api.js";
+
+export default {
+ components: { Controls },
+ name: "Home",
+ data() {
+   return {
+     roomUrl: "",
+     status: "home",
+     callFrame: null,
+     validRoomURL: false,
+     roomError: false,
+     runningLocally: false,
+   };
+ },
+ created() {
+   if (window?.location?.origin.includes("localhost")) {
+     this.runningLocally = true;
+   }
+ },
+ methods: {
+   createAndJoinRoom() {
+     api
+       .createRoom()
+       .then((room) => {
+         this.roomUrl = room.url;
+         this.joinRoom(room.url);
+       })
+       .catch((e) => {
+         console.log(e);
+         this.roomError = true;
+       });
+   },
+   // Daily callframe created and joined below
+   joinRoom(url) {
+     if (this.callFrame) {
+       this.callFrame.destroy();
+     }
+
+     // Daily event callbacks
+     const logEvent = (ev) => console.log(ev);
+     const goToLobby = () => (this.status = "lobby");
+     const goToCall = () => (this.status = "call");
+     const leaveCall = () => {
+       if (this.callFrame) {
+         this.status = "home";
+         this.callFrame.destroy();
+       }
+     };
+     // DailyIframe container element
+     const callWrapper = this.$refs.callRef;
+
+     // Create Daily call
+     const callFrame = DailyIframe.createFrame(callWrapper, {
+       iframeStyle: {
+         height: "auto",
+         width: "100%",
+         aspectRatio: 16 / 9,
+         minWidth: "400px",
+         maxWidth: "920px",
+         border: "1px solid var(--grey)",
+         borderRadius: "4px",
+       },
+       showLeaveButton: true,
+     });
+     this.callFrame = callFrame;
+
+     // Add event listeners and join call
+     callFrame
+       .on("loaded", logEvent)
+       .on("started-camera", logEvent)
+       .on("camera-error", logEvent)
+       .on("joining-meeting", goToLobby)
+       .on("joined-meeting", goToCall)
+       .on("left-meeting", leaveCall);
+
+     callFrame.join({ url });
+   },
+   submitJoinRoom() {
+     this.joinRoom(this.roomUrl);
+   },
+   validateInput(e) {
+     this.validRoomURL = !!this.roomUrl && e.target.checkValidity();
+   },
+ },
+};
+</script>
