6a4fd98335731312ecc0c52ea604e61c94dacaa2
Jocelyn Tan <jocelynpeixuan@gmail.com>
2021-10-14 03:10:32 +0800

    created counsellor's login pg, patient's signup and login pg, logout func
    - counsellor's login page: account manually set up in firebase
    - patient's signup and login
    - logout function
    - patient and counsellor profile takes in info from firebase

--- src/components/NavBarCounsellor.vue
+++ src/components/NavBarCounsellor.vue
@@ -1,37 +1,45 @@
 <template>
-    <div id="logged" >
+    <div id="logged" v-if="user">
         <div id="nav">
             <router-link to="/counsellorProfile">My Profile</router-link> | 
             <router-link to="/myPatients">My Patients</router-link> | 
             <router-link to="/about">About Us</router-link> | 
-            <router-link to="/helpResources">Help Resources</router-link>
+            <router-link to="/helpResources">Help Resources</router-link> | 
+            <Logout/>
             <!-- <router-link to="/logout">Logout</router-link> -->
         </div>
     </div>
 </template>
 
-// <script>
-// import { getAuth, onAuthStateChanged } from 'firebase/auth';
+<script>
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import Logout from '@/components/Logout.vue'
 
-// export default {
-//     name:'NavBarPatient',
+export default {
+    name:'NavBarCounsellor',
+    components:{
+        Logout
+    },
 
-//     data() {
-//         return {
-//             user:false,
-//         }     
-//     },
+    data() {
+        return {
+            user:false,
+        }     
+    },
 
-//     mounted() {
-//         const auth = getAuth();
-//         onAuthStateChanged(auth, (user) => {
-//             if (user) {
-//                 this.user = user;
-//             }
-//         })
-//     }
-// }
-// </script>
+    mounted() {
+        const auth = getAuth();
+        // var user = firebase.auth().currentUser;
+        // await db.collection("Counsellors").doc(k).get()
+
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                this.user = user;
+            }
+        })
+    }
+}
+</script>
 
 <style scoped>
 
--- src/components/NavBarPatient.vue
+++ src/components/NavBarPatient.vue
@@ -1,10 +1,11 @@
 <template>
     <div id="logged" v-if="user">
         <div id="nav">
-            <router-link to="/patientProfile">My Profile</router-link>
-            <router-link to="/findCounsellor">Find a Counsellor</router-link>
-            <router-link to="/about">About Us</router-link>
-            <router-link to="/helpResources">Help Resources</router-link>
+            <router-link to="/patientProfile">My Profile</router-link> | 
+            <router-link to="/findcounsellor">Find A Counsellor</router-link> | 
+            <router-link to="/about">About Us</router-link> |
+            <router-link to="/helpresources">Help Resources</router-link> |  
+            <Logout/>
             <!-- <router-link to="/logout">Logout</router-link> -->
         </div>
     </div>
@@ -12,9 +13,13 @@
 
 <script>
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import Logout from '@/components/Logout.vue'
 
 export default {
     name:'NavBarPatient',
+    components:{
+        Logout
+    },
 
     data() {
         return {
@@ -35,16 +40,17 @@ export default {
 
 <style scoped>
 #nav {
-    padding:30px;
-    text-align:center;
+  text-align: right;
+  /* float: right; */
+  margin-right: 50px; 
 }
 
-#nav a {
-    font-weight: bold;
-    color: darkblue;
+#nav a{
+  font-weight: bold;
+  color:lightslategray;
 }
 
-#nav a.router-link-exact-active{
-    color: purple;
+#nav a.router-link-exact-active {
+  color:black;
 }
 </style>
