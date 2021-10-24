ab41c56164a0000ccd6fbf49c4e152e460310bf8
shu-hui-ng <shuhui.ng@u.nus.edu>
2021-10-25 03:33:37 +0800

    configure counsellor profile pov

--- src/components/CounsellorCalendar.vue
+++ src/components/CounsellorCalendar.vue
@@ -1,40 +1,90 @@
 <template>
-    <div>
-        <v-calendar :attributes="attributes" @dayclick="onDayClick"/>
-        <div v-if="upcoming.length != 0">
-            <h3>Upcoming Sessions</h3>
-            <div v-for="item in upcoming" :key="item">
-                {{ item.date }} {{ item.time }}
+    <div v-if="user">
+        <div v-if="this.user_type=='patient'">
+            <v-calendar :attributes="attributes" @dayclick="onDayClick"/>
+            <div v-if="avail.length != 0">
+                <h3>Available Sessions</h3>
+                <div v-for="item in avail" :key="item">
+                    {{ item.date }} {{ item.time }}
+                    <button v-on:click="book(this.counsellor_ID, item)">Book</button>
+                </div>
+            </div>
+            <div v-else>
+                <h4>No session for selected day(s)</h4>
             </div>
         </div>
-        <div v-if="avail.length != 0">
-            <h3>Available Sessions</h3>
-            <div v-for="item in avail" :key="item">
-                {{ item.date }} {{ item.time }}
-            </div>
+
+        <div v-if="this.user_type=='counsellor'">
+                <div v-if="createSession == true">
+                    <v-date-picker v-model="date" mode="dateTime"/>
+                    <br><br>
+                    <button v-on:click="create()">Create Session</button>
+                    <button v-on:click="createSession = false">Back to appointments calendar</button>
+                </div>
+                <div v-else>
+                    <v-calendar :attributes="attributes" @dayclick="onDayClick"/>
+                    <div v-if="upcoming.length != 0">
+                        <h3>Upcoming Sessions</h3>
+                        <div v-for="item in upcoming" :key="item">
+                            {{ item.date }} {{ item.time }}
+                        </div>
+                    </div>
+                    <div v-if="avail.length != 0">
+                        <h3>Available Sessions</h3>
+                        <div v-for="item in avail" :key="item">
+                            {{ item.date }} {{ item.time }} 
+                        </div>
+                    </div>
+                    <div v-if="upcoming.length == 0 && avail.length == 0 && days.length!=0">
+                        <h4>No session for selected day(s)</h4>
+                    </div>
+                    <br><br>
+                    <button v-on:click="createSession = true">Add New Session</button>
+                </div>
         </div>
-        <div v-if="upcoming.length == 0 && avail.length == 0 && days.length!=0">
-            <h4>No session for selected day(s)</h4>
-        </div>
-        <br><br>
-        <button><router-link to="/addNewSession">Add New Session</router-link></button>
     </div>
 </template>
 
 <script>
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import firebaseApp from '../firebase.js';
 import { getFirestore } from 'firebase/firestore'
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 const db = getFirestore(firebaseApp);
 
 export default {
     name:"CounsellorCalendar",
     data() {
         return {
+        user:false,
+        fbuser:"",
+        user_type:"patient",
+        counsellor_ID: this.$route.params.id,
         days: [],
         upcoming: [],
-        avail: []
+        avail: [],
+        booked: [],
+        createSession: false,
+        date: new Date()
         };
+    },
+    mounted() {
+        const auth = getAuth();
+        onAuthStateChanged(auth, user => {
+            if (user) {
+                this.user = user;
+                if (user.user_type == "counsellor") {
+                    this.user_type = "counsellor";
+                }
+                this.fbuser = user.email;
+                this.getDetails(this.fbuser);
+                // this.updateCurrentlyAvailable();
+                // this.avgRating(this.fbuser);
+                // this.mountedCheckCurrentlyAvailable();
+                console.log("bottom of mounted()");
+                // console.log("time is ", Timestamp.valueOf(Timestamp.now()))
+            }
+        })
     },
     computed: {
         dates() {
@@ -49,56 +99,112 @@ export default {
     },
     methods: {
         async onDayClick(day) {
-        const idx = this.days.findIndex(d => d.id === day.id)
-        const docRef = doc(db, "Counsellors", "john@gmail.com")
-        const docSnap = await getDoc(docRef);
+            const idx = this.days.findIndex(d => d.id === day.id)
+            const docRef = doc(db, "Counsellors", this.counsellor_ID)
+            const docSnap = await getDoc(docRef);
 
-        let z = docSnap.data()
-        var avail = z.available_slots
-        var upcoming = z.upcoming_counsellor_sessions
+            let z = docSnap.data()
+            var avail = z.available_slots
+            var upcoming = z.upcoming_counsellor_sessions
 
-        if (idx >= 0) {
-            this.days.splice(idx, 1)
-            this.upcoming.splice(idx, upcoming.length)
-            this.avail.splice(idx, avail.length)
-        } else {
-            this.days.push({
-            id: day.id,
-            date: day.date,
-            })
+            if (idx >= 0) {
+                this.days.splice(idx, 1)
+                this.upcoming.splice(idx, upcoming.length)
+                this.avail.splice(idx, avail.length)
+            } else {
+                this.days.push({
+                id: day.id,
+                date: day.date,
+                })
 
-            upcoming.forEach(async (s) => {
-                const sessionRef = doc(db, "Sessions", s)
-                const sessionSnap = await getDoc(sessionRef)
-                let session = sessionSnap.data().session_time
+                upcoming.forEach(async (s) => {
+                    const sessionRef = doc(db, "Sessions", s)
+                    const sessionSnap = await getDoc(sessionRef)
+                    let session = sessionSnap.data().session_time
 
-                let a = session.toDate()
-                let a_date = a.toDateString()
-                let a_time = a.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1")
-                let a_gmtDate = new Date(a.setHours(a.getHours() + 8))
+                    let a = session.toDate()
+                    let a_date = a.toDateString()
+                    let a_time = a.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1")
+                    let a_gmtDate = new Date(a.setHours(a.getHours() + 8))
 
-                if (a_gmtDate.toISOString().substr(0,10) == day.id) {
-                    this.upcoming.push({
-                        date: a_date,
-                        time: a_time
-                    })
-                }
-            })
+                    if (a_gmtDate.toISOString().substr(0,10) == day.id) {
+                        this.upcoming.push({
+                            date: a_date,
+                            time: a_time,
+                        })
+                    }
+                })
 
-            avail.forEach((slot) => {
-                let s = slot.toDate()
-                let s_date = s.toDateString()
-                let s_time = s.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1")
-                let s_gmtDate = new Date(s.setHours(s.getHours() + 8))
+                avail.forEach(async (x) => {
+                    const slotRef = doc(db, "Sessions", x)
+                    const slotSnap = await getDoc(slotRef)
+                    let slot = slotSnap.data().session_time
+                    
+                    let s = slot.toDate()
+                    let s_date = s.toDateString()
+                    let s_time = s.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1")
+                    let s_gmtDate = new Date(s.setHours(s.getHours() + 8))
 
-                if (s_gmtDate.toISOString().substr(0,10) == day.id) {
-                    this.avail.push({
-                        date: s_date,
-                        time: s_time
-                    })
-                }
-            })
-        }
+                    if (s_gmtDate.toISOString().substr(0,10) == day.id) {
+                        let s_originalDate = new Date(s_gmtDate.setHours(s_gmtDate.getHours() - 8 ))
+                        this.avail.push({
+                            date: s_date,
+                            time: s_time,
+                            session: this.counsellor_ID+s_originalDate
+                        })
+                    }
+                })
+
+
+            }
+        },
+
+        async book(counsellor, item){
+           //edit counsellor > remove from available_slots and add to upcoming_counsellor_sessions
+           const counsellorRef = doc(db, "Counsellors", this.counsellor_ID)
+            const counsellorSnap = await getDoc(counsellorRef)
+            let z = counsellorSnap.data()
+            var avail = z.available_slots
+            var idx = avail.indexOf(item.session)
+            avail.splice(idx, 1)
+            var upcoming = z.upcoming_counsellor_sessions
+            upcoming.push(item.session)
+            await setDoc(counsellorRef, {upcoming_counsellor_sessions: upcoming, available_slots: avail}, {merge: true})
+           
+           //edit session > set user_email
+            await setDoc(doc(db, "Sessions", item.session), {user_email: this.fbuser}, {merge: true})
+            
+            //edit patient > add to upcoming_user_sessions
+            const patientRef = doc(db, "Patients", this.fbuser)
+            const patientSnap = await getDoc(patientRef)
+            let y = patientSnap.data()
+            var patient_upcoming = y.upcoming_user_sessions
+            patient_upcoming.push(item.session)
+            await setDoc(doc(db, "Patients", this.fbuser), {upcoming_user_sessions: patient_upcoming}, {merge: true})
+
+            alert("New appointment booked for " + item.time + " " + item.date + "!")
+            location.reload()
+        },
+
+        async create(){
+            //edit counsellor > add to available_slots
+            const docRef = doc(db, "Counsellors", this.counsellor_ID)
+            const docSnap = await getDoc(docRef)
+            let z = docSnap.data()
+            var avail = z.available_slots
+            avail.push(this.counsellor_ID+this.date)
+            await setDoc(docRef, {available_slots: avail}, {merge: true})
+
+            //create new session
+            await setDoc(doc(db, "Sessions", this.counsellor_ID+this.date), {
+                counsellor_email: this.counsellor_ID,
+                rating: null,
+                room_ID: "",
+                session_notes: "",
+                session_time: this.date,
+                user_email: ""
+            });
+            alert("New session created for " + this.date + " !")
         }
     }
 }
