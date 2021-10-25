c6cfff2839632a31412cc108773b7104f32383bc
Jocelyn Tan <jocelynpeixuan@gmail.com>
2021-10-25 18:27:32 +0800

    edit profile picture pop up

---
+++ src/components/CounsellorProfilePicModal.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="modal-overlay" @click="$emit('close-modal')">
+    <div class="modal" @click.stop>
+      <h6>Edit Profile Picture</h6>
+      <p>Click anywhere outside this pop-up to exit.</p>
+
+    <div>
+        <label for="formFile">Upload Image:</label>
+        
+        <input class="form-control" ref="fileInput" type="file" @input="preview">
+        
+        <div class="imagePreviewWrapper" :style="{ 'background-image': `url(${previewImage})` }" @click="selectImage"></div>
+        
+        <div>
+            <button @click="uploadImage(this.user)">Upload</button>
+        </div>
+    </div>
+    </div>
+    <div class="close" @click="$emit('close-modal')">
+    </div>
+  </div>
+</template>
+
+<script scoped>
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import firebaseApp from '@/firebase.js';
+import { getFirestore } from "firebase/firestore"
+import { doc, updateDoc } from "firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+const db = getFirestore(firebaseApp);
+const storage = getStorage(firebaseApp);
+
+export default {
+    name:"CounsellorProfilePicModal" ,
+
+    data(){
+        return{
+            user:false,
+            email:"",
+            user_type:"patient",
+            fbuser:"",
+            previewImage: null,
+            imageData:null,
+        }
+    },
+
+    mounted() {
+        const auth = getAuth();
+        onAuthStateChanged(auth, user => {
+            if (user) {
+                this.user = user;
+                if (user.user_type == "counsellor") {
+                    this.user_type = "counsellor";
+                }
+                this.fbuser = user.email;
+            }
+        })
+    },
+
+    methods: {
+        selectImage() {
+            this.$refs.fileInput.click()
+        },
+
+        preview() {
+            console.log("preview")
+            let input = this.$refs.fileInput
+            let file = input.files
+            this.imageData = event.target.files[0]
+            console.log("preview3")
+            if (file && file[0]) {
+                console.log("preview4")
+                let reader = new FileReader
+                reader.onload = e => {
+                    this.previewImage = e.target.result
+                    console.log("preview5")
+            }
+            console.log("preview6")
+            reader.readAsDataURL(file[0])
+            console.log("preview7")
+            this.$emit('input', file[0])
+            console.log("preview done")
+            }
+        },
+
+        async uploadImage(user) {
+            console.log("upload image")
+            const picRef = ref(storage, 'counsellorProfilePic');
+            let storageRef=ref(picRef, `${this.user.email}`);
+
+            if (this.imageData!=null) {
+                let snapshot = await uploadBytes(storageRef, this.imageData)
+                this.uploadValue = (snapshot.bytesTransferred/snapshot.totalBytes)*100
+                
+                let url = await getDownloadURL(storageRef)
+                const docRef = doc(db, "Counsellors", user.email);
+                updateDoc(docRef, {
+                    "profile_pic": url.toString()
+                }).then (
+                    alert("Profile picture uploaded successfully! Please wait a few seconds for the page to reload."),
+                    await new Promise((resolve) => setTimeout(resolve,800)),
+                    location.reload(),
+                ).catch(e => {
+                    console.log(e)
+                })
+            } else {
+                alert("Please select an image.")
+            }
+            
+        },
+      }
+}
+</script>
+
+<style scoped>
+
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  background-color: #000000da;
+  z-index: 1;
+}
+
+.modal {
+  text-align: center;
+  background-color: white;
+  height: 500px;
+  width: 500px;
+  margin-top: 10%;
+  padding: 60px 0;
+  border-radius: 20px;
+}
+.close {
+  margin: 10% 0 0 16px;
+  cursor: pointer;
+}
+
+.close-img {
+  width: 25px;
+}
+
+.check {
+  width: 150px;
+}
+
+h6 {
+  font-weight: 500;
+  font-size: 28px;
+  margin: 20px 0;
+}
+
+p {
+  font-size: 16px;
+  margin: 20px 0;
+}
+
+button {
+  background-color: #ac003e;
+  width: 150px;
+  height: 40px;
+  color: white;
+  font-size: 14px;
+  border-radius: 16px;
+  /* margin-top: 50px; */
+}
+.imagePreviewWrapper {
+    background-repeat: no-repeat;
+    /* border-radius: 50%; */
+    /* border-color: black; */
+    width: 200px;
+    height: 200px;
+    display: block;
+    cursor: pointer;
+    margin: 0 auto 30px;
+    background-size: contain;
+    background-position: center center;
+    margin-top: 10px;
+}
+</style>
--- src/views/Counsellor/CounsellorProfile.vue
+++ src/views/Counsellor/CounsellorProfile.vue
@@ -6,15 +6,16 @@
 
     <div v-if="user"> 
         <br><br>
-        <div v-if ="this.user_type == 'counsellor'">
+        <!-- <div v-if ="this.user_type == 'counsellor'"> -->
             <NavBarCounsellor/>
-        </div>
-        <div v-else>
+        <!-- </div> -->
+        <!-- <div v-else>
             <NavBarPatient/>
-        </div>
+        </div> -->
         <div style="text-align:center;">
             <h1>Counsellor Profile</h1>
-            <div v-if ="this.user_type =='counsellor'">
+            <!-- <div v-if ="this.user_type =='counsellor'"> -->
+            <div>
                 <!-- Need a toggle button for currently_available -->
                 <div class = "toggle">
                     <button id = "toggleButton" @click="toggleCurrentlyAvailable">Click to toggle current availability on/off</button>
@@ -30,67 +31,15 @@
                 <div id="bgBlock"> 
                     <div id="col-1">
                         <div id="counsellorDetails"> 
-                        
-                        <div v-if="this.user_type=='counsellor'">
-                            <label for="formFile">Upload Image:</label>
-                            
-                            <input class="form-control" ref="fileInput" type="file" @input="preview">
-                            
-                            <div class="imagePreviewWrapper" :style="{ 'background-image': `url(${previewImage})` }" @click="selectImage"></div>
-                            
-                            <div>
-                                <button @click="uploadImage(this.user)">upload</button>
-                            </div>
-                        </div>
 
-                        <!-- <div>
-                            <div class="text-center">
-                                <button @click="create">upload</button>
-                            </div>
-                        </div> -->
                             <div v-if="this.profile_pic">
                                 <img id="profilepic" :src='this.profile_pic'>
                             </div>
 
-                            <!-- <div class="container p-5">
-                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                    Edit Profile Picture
-                                </button>
 
-                                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title text-danger" id="exampleModalLabel">Edit Profile Picture</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-
-                                            <div class="modal-body">
-                                                <div class="mb-3">
-                                                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                                                </div>
-                                                <div class="mb-3">
-                                                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                                                    <input type="password" class="form-control" id="exampleInputPassword1">
-                                                </div>
-                                                <div class="mb-3">
-                                                <label for="formFile">Upload Image:</label>
-                                                <input class="form-control" ref="fileInput" type="file" @input="preview">
-                                                <br>
-                                                <button @click="uploadImage(this.user)" class="btn btn-primary">Upload</button>
-                                                <div class="imagePreviewWrapper" :style="{ 'background-image': `url(${previewImage})` }" @click="selectImage"></div>
-                                                </div>
-                                            </div>
-
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Close</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div> -->
+                            <div class="save-btn">
+                                <button @click="showModal = true">Edit Profile Picture</button>
+                            </div>
 
                             <p> Name: <strong>{{this.name}}</strong><br>
                             Email: <strong>{{this.email}}</strong><br>
@@ -111,25 +60,25 @@
                 </div>
         </div>
     </div>
+    <CounsellorProfilePicModal v-show="showModal" @close-modal="showModal = false" />
 </template>
 
 <script scoped>
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import NavBarCounsellor from "@/components/NavBarCounsellor.vue"
-import NavBarPatient from '@/components/NavBarPatient.vue'
+// import NavBarPatient from '@/components/NavBarPatient.vue'
 import CounsellorCalendar from "@/components/CounsellorCalendar.vue"
 import AlertCounsellorSession from '@/components/AlertCounsellorSession.vue'
+import CounsellorProfilePicModal from "@/components/CounsellorProfilePicModal.vue"
 
 import firebaseApp from '@/firebase.js';
 import { getFirestore } from "firebase/firestore"
 import { doc, updateDoc, getDoc, Timestamp } from "firebase/firestore";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 const db = getFirestore(firebaseApp);
-const storage = getStorage(firebaseApp);
 
 export default {
-    components: {NavBarCounsellor, NavBarPatient, CounsellorCalendar,AlertCounsellorSession},
+    components: {NavBarCounsellor,CounsellorCalendar,AlertCounsellorSession,CounsellorProfilePicModal},
     name:"CounsellorProfile" ,
 
     data(){
@@ -145,9 +94,8 @@ export default {
             ratings:[],
             avgRatings:"",
             specialisations_formatted:"",
-            previewImage: null,
             profile_pic: "",
-            imageData:null,
+            showModal: false,
         }
     },
 
@@ -275,77 +223,16 @@ export default {
             return false;
         },
 
-        selectImage() {
-            this.$refs.fileInput.click()
-        },
-
-        preview() {
-            console.log("preview")
-            let input = this.$refs.fileInput
-            let file = input.files
-            this.imageData = event.target.files[0]
-            console.log("preview3")
-            if (file && file[0]) {
-                console.log("preview4")
-                let reader = new FileReader
-                reader.onload = e => {
-                    this.previewImage = e.target.result
-                    console.log("preview5")
-            }
-            console.log("preview6")
-            reader.readAsDataURL(file[0])
-            console.log("preview7")
-            this.$emit('input', file[0])
-            console.log("preview done")
-            }
-        },
-
-        async uploadImage(user) {
-            console.log("upload image")
-            const picRef = ref(storage, 'counsellorProfilePic');
-            let storageRef=ref(picRef, `${this.user.email}`);
-
-            if (this.imageData!=null) {
-                let snapshot = await uploadBytes(storageRef, this.imageData)
-                this.uploadValue = (snapshot.bytesTransferred/snapshot.totalBytes)*100
-                
-                let url = await getDownloadURL(storageRef)
-                const docRef = doc(db, "Counsellors", user.email);
-                updateDoc(docRef, {
-                    "profile_pic": url.toString()
-                }).then (
-                    alert("Profile picture uploaded successfully! Please wait a few seconds for the page to reload."),
-                    await new Promise((resolve) => setTimeout(resolve,800)),
-                    location.reload(),
-                ).catch(e => {
-                    console.log(e)
-                })
-            } else {
-                alert("Please select an image.")
-            }
-            
-        },
     }
 }
 </script>
 
 <style scoped>
-.imagePreviewWrapper {
-    background-repeat: no-repeat;
-    width: 230px;
-    height: 230px;
-    display: block;
-    cursor: pointer;
-    margin: 0 auto 30px;
-    background-size: contain;
-    background-position: center center;
-}
 #profilepic {
     border-radius: 50%;
     margin-top: 10px;
     width: 230px;
     height: 230px;
-    margin-bottom: -20px;
 }
 #bgBlock {
     display:flex;
