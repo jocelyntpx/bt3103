ac0b6151d50330f7cfe685a86b2fb85c84c93170
Jocelyn Tan <jocelynpeixuan@gmail.com>
2021-10-30 20:42:26 +0800

    notfound page navbar, show/hide password function

--- src/views/General/NotFound.vue
+++ src/views/General/NotFound.vue
@@ -1,39 +1,80 @@
 <template>
-    <div style="text-align:center;" v-if="user">
-        <NavBarPatient/>
-        <h3>Page Not Found!!></h3>
-        <h4>Please click on the above available options</h4>
-        <Logout/>
-        <br>
+    <br><br>
+    <div v-if="user">
+        <div v-if = (this.counsellor)>
+            <NavBarCounsellor/>
+        </div>
+        <div v-else>
+            <NavBarPatient/>
+        </div>
+    </div>
+    <div v-else>
+        <NavBarGeneral/>
+    </div>
+    <br>
+
+    <div style="text-align:center;">
+        <h3>Page Not Found!!</h3>
+        <h4>Please click on the above available options in the navigation bar.</h4>
+        <br> 
     </div>
 </template>
 
 <script>
 import NavBarPatient from '@/components/NavBarPatient.vue'
-import Logout from '@/components/Logout.vue'
+import NavBarCounsellor from '@/components/NavBarCounsellor.vue'
+import NavBarGeneral from '@/components/NavBarGeneral.vue'
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+
+import firebaseApp from '../../firebase.js';
+import { getFirestore } from "firebase/firestore"
+import { doc, getDoc } from "firebase/firestore";
+const db = getFirestore(firebaseApp);
 
 export default {
     name:"NotFound",
 
     components:{
         NavBarPatient,
-        Logout
+        NavBarCounsellor,
+        NavBarGeneral,
     },
 
     data() {
         return {
             user:false,
+            counsellor: false,
         }     
     },
 
     mounted() {
         const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
+        onAuthStateChanged(auth, async user => {
             if (user) {
                 this.user = user;
+                this.fbuser = user.uid;
+
+            this.check = await this.isPatient(auth, this.fbuser);
             }
         })
     },
+
+    methods: {
+        async isPatient(auth, user) {
+            let docRef = doc(db, "Counsellors", String(user));
+            let counsellorDoc = await getDoc(docRef);
+                
+            if (counsellorDoc.exists()) {
+                console.log('Is counsellor!');
+                this.counsellor = true;
+                return false
+            } else {
+                console.log('Is patient!');
+                this.counsellor = false;
+                return true
+            }
+        },
+    }    
 }
 </script>
